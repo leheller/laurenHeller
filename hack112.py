1a88fc4cc4027b232a3d9f432742e6f00a87e093
leheller <49327345+leheller@users.noreply.github.com>
2019-04-05 21:34:26 -0400

    Add files via upload

--- hack112.py
+++ hack112.py
@@ -1,1 +1,8 @@
-print("hack112 is awesome")
+#Produces a huge ass picture
+import cv2
+img = cv2.imread("puppy.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(gray,47,255,cv2.THRESH_BINARY)
+cv2.imshow("thresh",thresh) #<-- grayscale image
+#cv2.imshow("original",img)  <-- original image
+cv2.waitKey(0)
